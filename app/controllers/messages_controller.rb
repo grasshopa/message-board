17cb72f4a0b622ab827b3411786be11b17ac51d0
grasshopa <coucicouca.web@gmail.com>
2016-11-29 09:46:52 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
+  #メッセージを新規作成する際のフォームを表示するindexアクション
   def index
+    #Messageモデルを初期化して@messageに代入→新規作成フォームが表示
+    @message = Message.new
+    #Messageを全て取得
+    @messages = Message.all
   end
+  
+  #フォームからパラメータを受け取りDBに格納するcreateアクション
+  def create
+    #パラメータを受取り、Messageインスタンスを生成(Message.new)、@messageに代入
+    @message = Message.new(message_params)
+    #メッセージモデルのインスタンスをデータベースに保存
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  ##ここまで
 end
